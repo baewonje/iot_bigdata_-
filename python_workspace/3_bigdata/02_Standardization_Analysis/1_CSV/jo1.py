4472426d19e8ae87962619d5b3b6d42011832655
baewonje <50129576+baewonje@users.noreply.github.com>
2019-12-06 17:19:13 +0900

    2019-11-22

--- python_workspace/3_bigdata/02_Standardization_Analysis/1_CSV/jo1.py
+++ python_workspace/3_bigdata/02_Standardization_Analysis/1_CSV/jo1.py
@@ -2,7 +2,12 @@
 import sys
 import os
 import csv
+import pandas as pd
+import glob
 
+
+
+listnum = 0
 id = "ID"
 repository_name = 'clothes_repository'
 file_name = 'clothes_save_list'
@@ -12,6 +17,79 @@ dir_delimeter = '/'
 is_header = False
 is_first = False
 initial_file_name = f'{repository_name}{dir_delimeter}{file_name}1.{file_format}'
+
+Crud_Menu="""
+1. 옷 저장
+2. 옷 조회
+3. 옷 수정
+4. 옷 삭제
+입력하세요: """
+
+Select_Menu="""
+1. 전체 조회
+2. 상의 조회
+3. 하의 조회
+5. 아우터 조회
+입력 하세요: """
+
+Clothes_Menu="""
+1. 상의
+2. 하의
+3. 아우터
+입력하세요: """
+
+Top_Menu="""
+1. 티셔츠
+2. 셔츠
+3. 맨투맨
+4. 후드티
+5. 블라우스
+6. 니트
+7. 폴라티
+입력하세요: """
+
+Bottom_Menu="""
+1. 면바지
+2. 청바지
+3. 슬랙스
+4. 반바지
+5. 치마
+6. 원피스
+입력하세요: """
+
+Outer_Menu="""
+1. 코트
+2. 패딩
+3. 자켓
+4. 후드집업
+5. 바람막이
+6. 가디건
+7. 야상
+입력하세요: """
+
+Length_Menu="""
+1. 반팔
+2. 긴팔
+3. 추가옵션
+입력하세요: """
+
+def color_choice():
+    sort_color = int(input(" 1. 빨강\n 2. 주황\n 3. 노랑\n 4. 초록 \n"
+                           " 5. 파랑 \n 6. 남색\n 7. 보라\n 8. 블랙\n 9. 흰색\n 입력하세요: "))
+    return sort_color
+def all_select():
+    # supplier_data.csv
+    # output_files/9output_pandas.csv
+    input_path = sys.argv[1]
+
+    all_files = glob.glob(os.path.join(input_path, 'clothes_save_list*'))
+
+    all_data_frames = []
+    for file in all_files:
+        data_frame = pd.read_csv(file, index_col=None)
+        all_data_frames.append(data_frame)
+    data_frame_concat = pd.concat(all_data_frames, axis=0, ignore_index=True)
+    print(data_frame_concat)
 
 
 def get_dest_file_name(file_index):
@@ -56,49 +134,135 @@ def file_count():
     return index
 
 def sort_classification(sort1,sort2,sort3,sort_color):
-    global sort_list
+    global sort_list,listnum
+    listnum += 1
 
     if sort1 == 1:
-        sort1 = "Top"
+        sort1 = "top"
     elif sort1 == 2:
-        sort1 = "Bottoms"
-    elif Sort1 == 3:
-        sort1 = "Outer"
+        sort1 = "bottoms"
+    elif sort1 == 3:
+        sort1 = "outer"
+
     if sort2 == 1:
         sort2 = "T-shirt"
+    elif sort2 == 2:
+        sort2 = "shirt"
+    elif sort2 == 3:
+        sort2 = "man-to-man"
+    elif sort2 == 4:
+        sort2 = "hoodie"
+    elif sort2 == 5:
+        sort2 = "blouse"
+    elif sort2 == 6:
+        sort2 = "knitwear"
+    elif sort2 == 7:
+        sort2 = "turtleneck"
+    elif sort2 == 8:
+        sort2 = "cotton"
+    elif sort2 == 9:
+        sort2 = "jeans"
+    elif sort2 == 10:
+        sort2 = "slacks"
+    elif sort2 == 11:
+        sort2 = "breeches"
+    elif sort2 == 12:
+        sort2 = "skirt"
+    elif sort2 == 13:
+        sort2 = "dress"
+    elif sort2 == 14:
+        sort2 = "coat"
+    elif sort2 == 15:
+        sort2 = "padding"
+    elif sort2 == 16:
+        sort2 = "jacket"
+    elif sort2 == 17:
+        sort2 = "hood zip-up"
+    elif sort2 == 18:
+        sort2 = "Windbreaker"
+    elif sort2 == 19:
+        sort2 = "cardigan"
+    elif sort2 == 20:
+        sort2 = "Field jacket"
+
     if sort3 == 1:
         sort3 = "short"
+    elif sort3 == 2:
+        sort3 = "long"
+    elif sort3 == 2:
+        sort3 = "Additional-options"
+
     if sort_color == 1:
         sort_color = "Red"
-    sort_list = [sort1,sort2,sort3,sort_color]
+    elif sort_color == 2:
+        sort_color = "orange"
+    elif sort_color == 3:
+        sort_color = "yellow"
+    elif sort_color == 4:
+        sort_color = "green"
+    elif sort_color == 5:
+        sort_color = "blue"
+    elif sort_color == 6:
+        sort_color = "navy"
+    elif sort_color == 7:
+        sort_color = "violet"
+    elif sort_color == 8:
+        sort_color = "black"
+    elif sort_color == 9:
+        sort_color = "white"
+
+    sort_list = [listnum,id,sort1,sort2,sort3,sort_color]
 
 
 while True:
     sort_list =[]
     file_size = 0
-    sort1 = int(input(" 1. 상의\n 2. 하의\n 3. 아우터\n 입력하세요: "))
-
-    if sort1 == 1:
-        sort2 = int(input(" 1. 티셔츠\n 2. 셔츠\n 3. 맨투맨 \n 4. 후드티 \n"
-                          " 5. 블라우스\n 6. 니트\n 7. 폴라티\n 입력하세요: "))
-        if sort2 == 1:
-            sort3 = int(input(" 1. 반팔\n 2. 긴팔 \n 3. 추가옵션\n 입력하세요: "))
-            sort_color = int(input(" 1. 빨강\n 2. 주황\n 3. 노랑\n 4. 초록 \n"
-                                   " 5. 파랑 \n 6. 남색\n 7. 보라\n 8. 블랙\n 9. 흰색\n 입력하세요: "))
-            print(sort1,sort2,sort3,sort_color)
-            sort_classification(sort1,sort2,sort3,sort_color)
+    crud = int(input(Crud_Menu))
+    if crud == 1:
+        sort1 = int(input(Clothes_Menu))
+        if sort1 == 1:
+            sort2 = int(input(Top_Menu))
+            if sort2 == 1:
+                sort3 = int(input(Length_Menu))
+                sort_classification(sort1,sort2,sort3,color_choice())
+                if not os.path.exists(repository_name):
+                    os.mkdir(repository_name)
+                if not os.path.exists(initial_file_name):
+                    is_first = True
+                    short_t_shirt(1,sort_list)
+                else:
+                    short_t_shirt(file_count(),sort_list)
+        elif sort1 == 2:
+            sort2_1 = int(input(Bottom_Menu))
+            sort2_1 += 7
+            sort_classification(sort1, sort2_1,0, color_choice())
             if not os.path.exists(repository_name):
                 os.mkdir(repository_name)
             if not os.path.exists(initial_file_name):
                 is_first = True
-                short_t_shirt(1,sort_list)
+                short_t_shirt(1, sort_list)
             else:
-                short_t_shirt(file_count(),sort_list)
-    elif sort1 == 2:
-        sort2_1 = int(input(" 1. 면바지 \n 2. 청바지 \n3. 슬랙스 \n 4. 반바지 "
-                            "\n5. 치마\n 6. 원피스\n 입력하세요: "))
+                short_t_shirt(file_count(), sort_list)
 
-    elif sort1 == 3:
+        elif sort1 == 3:
+            sort2_2 = int(input(Outer_Menu))
+            sort2_2 += 13
+            sort_classification(sort1, sort2_2, 0, color_choice())
+            if not os.path.exists(repository_name):
+                os.mkdir(repository_name)
+            if not os.path.exists(initial_file_name):
+                is_first = True
+                short_t_shirt(1, sort_list)
+            else:
+                short_t_shirt(file_count(), sort_list)
+
+        else:
+            continue
+    elif crud == 2:
+        select_Menu = int(input(Select_Menu))
+        if(select_Menu == 1):
+            all_select()
+    elif crud == 3:
         pass
-    else:
-        continue
+    elif crud == 4:
+        pass
